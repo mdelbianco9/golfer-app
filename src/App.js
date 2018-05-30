174423eb0d6d5977ce71a8d8b72a528dbde82212
marcus delbianco <mdelbianco9@gmail.com>
2018-05-29 21:29:22 -0700

    Set up create-react-app and worked on style and structure of home page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Home from './Pages/Home';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+       <Home/>
+    );
+  }
+}
+
+export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+	// WHen using the {} you can only use simple one line expressions, nothing too crazy
+	return (
+		<div id="HomePage">
+			<div className="row">
+				<h1>g</h1>
+				<h6>olfer</h6>
+			</div>
+
+			<div className="row">
+				<button className="start-button">Start</button>
+			</div>
+		</div>		
+		)
+};
+
+
+export default Home;
